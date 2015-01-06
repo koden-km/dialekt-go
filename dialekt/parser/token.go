367a0eabcd223a5697250c14b7c717fedcf89e9f
Kevin Millar <koden.km@gmail.com>
2015-01-06 18:06:07 +1000

    Finished Token.

--- dialekt/parser/token.go
+++ dialekt/parser/token.go
@@ -1,16 +1,46 @@
 package dialekt
 
-const (
-    TOKEN_WILDCARD_CHARACTER = '*';
+const TokenWildcardCharacter = "*"
 
-    TOKEN_LOGICAL_AND   = 1;
-    TOKEN_LOGICAL_OR    = 2;
-    TOKEN_LOGICAL_NOT   = 3;
-    TOKEN_STRING        = 4;
-    TOKEN_OPEN_BRACKET  = 6;
-    TOKEN_CLOSE_BRACKET = 7;
+type TokenType int
+
+const (
+	TokenTypeLogicalAnd   TokenType = 1
+	TokenTypeLogicalOr    TokenType = 2
+	TokenTypeLogicalNot   TokenType = 3
+	TokenTypeString       TokenType = 4
+	TokenTypeOpenBracket  TokenType = 6
+	TokenTypeCloseBracket TokenType = 7
 )
 
 type Token struct {
-	// TODO
+	TokenType    TokenType
+	Value        string
+	StartOffset  int
+	EndOffset    int
+	LineNumber   int
+	ColumnNumber int
+}
+
+func NewToken(tokenType TokenType, value string, startOffset, endOffset, lineNumber, columnNumber int) *Token {
+	return &Token{tokenType, value, startOffset, endOffset, lineNumber, columnNumber}
+}
+
+func (token *Token) String() string {
+	switch token.TokenType {
+	case TokenTypeLogicalAnd:
+		return "AND operator"
+	case TokenTypeLogicalOr:
+		return "OR operator"
+	case TokenTypeLogicalNot:
+		return "NOT operator"
+	case TokenTypeString:
+		return "tag"
+	case TokenTypeOpenBracket:
+		return "open bracket"
+	case TokenTypeCloseBracket:
+		return "close bracket"
+	}
+
+	panic("Unknown type.")
 }
